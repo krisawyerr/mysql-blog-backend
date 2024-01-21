943dcdcb4af6deeedc30633a2f90e296f5d7bff6
krisawyerr <krisawyerrr@gmail.com>
2024-01-21 12:56:42 -0800

    deploy vercel

---
+++ controllers/post.js
@@ -0,0 +1,57 @@
+import { db } from "../db.js";
+import jwt from "jsonwebtoken";
+
+export const getPost = (req,res) => {
+    const q = req.query.cat ? "select * from posts where cat = ?" : "select * from posts";
+    db.query(q, [req.query.cat], (err, data) => {
+        if (err) return res.send(err);
+        return res.status(200).json(data);
+    })
+}
+
+export const getSinglePost = (req,res) => {
+    const q = "select p.id, `username`, `title`, `desc`, p.img, u.img as userImg, `cat`, `date` from users u join posts p on u.id = p.uid where p.id = ? ";
+    db.query(q, [req.params.id], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json(data[0]);
+    });
+};
+
+export const addPost = (req, res) => {
+    const q = "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
+    const values = [ req.body.title, req.body.desc, req.body.img, req.body.cat, req.body.date, req.body.uid ];
+    db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.json("Post has been created.");
+    });
+};
+
+
+export const deletePost = (req, res) => {
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE `id` = ?";
+    db.query(q, [postId], (err, data) => {
+        if (err) {
+            return res.status(500).json("Failed to delete post!");
+        }
+        if (data.affectedRows === 0) {
+            return res.status(403).json("You can delete only your post!");
+        }
+        return res.json("Post has been deleted!");
+    });
+};
+
+
+export const updatePost = (req, res) => {
+    const postId = req.params.id;
+    
+    const q =
+        "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=? WHERE `id` = ?";
+
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+
+    db.query(q, [...values, postId], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.json("Post has been updated.");
+    });
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import postRoutes from "./routes/post.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+
+const app = express()
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json())
+
+app.use("/api/post", postRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file
+  res.status(200).json(file.filename);
+});
+
+app.listen(8800, () => {
+    console.log("connected")
+})
